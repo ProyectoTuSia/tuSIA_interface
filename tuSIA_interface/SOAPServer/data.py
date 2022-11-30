eadaa11b4a273a683d14b2802749c3926d44290e
Julian ro <davidro2121@gmail.com>
2022-11-30 18:52:16 -0500

    changing ip address to dns

--- tuSIA_interface/SOAPServer/data.py
+++ tuSIA_interface/SOAPServer/data.py
@@ -5,7 +5,7 @@ from gql.transport.requests import RequestsHTTPTransport
 
 
 transport = RequestsHTTPTransport(
-    url="http://35.188.174.1/:80", verify=True, retries=3,)
+    url="http://tusia-proxy-ag-service.default.svc.cluster.local/:80", verify=True, retries=3,)
 
 client = Client(transport=transport, fetch_schema_from_transport=True)
 
